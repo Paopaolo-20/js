cb62d11a001ec7b3eeae4748ecdb8c2ecdd3129c
Paopaolo-20 <johnpaololuna.05@gmail.com>
2025-08-04 06:24:59 +0800

    update

--- js-es6-hof/05-function-factory/script.js
+++ js-es6-hof/05-function-factory/script.js
@@ -1,3 +1,19 @@
-// Starter examples (function output expectations are provided in README)
+// 1. makeMultiplier(factor): returns a function that multiplies by the factor
+const makeMultiplier = factor => number => number * factor;
 
-// Start coding here...
+// Example usage
+const double = makeMultiplier(2);
+console.log(double(5)); // 10
+
+const triple = makeMultiplier(3);
+console.log(triple(4)); // 12
+
+// 2. makeGreeter(greeting): returns a function that greets with the provided greeting
+const makeGreeter = greeting => name => `${greeting}, ${name}!`;
+
+// Example usage
+const sayHi = makeGreeter("Hi");
+console.log(sayHi("Mia")); // "Hi, Mia!"
+
+const sayHello = makeGreeter("Hello");
+console.log(sayHello("Liam")); // "Hello, Liam!"
