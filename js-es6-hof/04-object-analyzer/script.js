f29dca349c533e76552ada8949abc511ab60ac40
Paopaolo-20 <johnpaololuna.05@gmail.com>
2025-08-04 06:49:46 +0800

    updated

--- js-es6-hof/04-object-analyzer/script.js
+++ js-es6-hof/04-object-analyzer/script.js
@@ -7,4 +7,22 @@ const settings = {
   grammarCheck: true
 };
 
-// Start coding here...
+
+console.log("Enabled Settings:");
+Object.entries(settings).forEach(([key, value]) => {
+  if (value === true) {
+    console.log(key + ": true");
+  }
+});
+
+
+const trueCount = Object.values(settings).filter(value => value === true).length;
+console.log("Number of true settings:", trueCount);
+
+
+const filteredEntries = Object.entries(settings).filter(([key, value]) => value === true);
+console.log("Filtered Entries:", filteredEntries);
+
+
+const enabledSettings = Object.fromEntries(filteredEntries);
+console.log("Rebuilt Enabled Settings Object:", enabledSettings);
