0239f88c7a62b776c9d037ceaa5a4914be287251
Paopaolo-20 <johnpaololuna.05@gmail.com>
2025-08-04 06:45:30 +0800

    updated

--- js-es6-hof/01-student-grades-dashboard/script.js
+++ js-es6-hof/01-student-grades-dashboard/script.js
@@ -7,4 +7,31 @@ const students = [
   { name: "Ella" } // no grade, should use default
 ];
 
-// Start coding here...
+
+console.log("Uppercase Student Names:");
+students.forEach(student => {
+  console.log(student.name.toUpperCase());
+});
+
+
+console.log("\nStudents with grade ≥ 85:");
+const topStudents = students.filter(({ grade = 0 }) => grade >= 85);
+topStudents.forEach(student => {
+  console.log(`${student.name}: ${student.grade}`);
+});
+
+
+const totalGrade = students.reduce((sum, { grade = 0 }) => sum + grade, 0);
+const averageGrade = totalGrade / students.length;
+console.log(`\nAverage Grade: ${averageGrade.toFixed(2)}`);
+
+
+function displayStudent({ name, grade = 0 }) {
+  return `Student: ${name}, Grade: ${grade}`;
+}
+
+
+console.log("\nFormatted Student Display:");
+students.forEach(student => {
+  console.log(displayStudent(student));
+});
